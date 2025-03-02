b001d60d5cd1a1a090c124ba68fc470299952735
adarshupadhyay21 <adarsh.97200114@gmail.com>
2025-03-02 15:22:50 +0530

    ShopContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-// import Collections from "./pages/Collection";
+// import Collection from "./pages/Collection";
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
 // import Product from "./pages/Product";
@@ -11,7 +11,7 @@ import Home from "./pages/Home";
 // import Orders from "./pages/Orders";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/SearchBar";
 //import { Toaster } from "react-hot-toast";
 //import VerifyPayment from "./pages/VerifyPayment";
 
@@ -20,13 +20,13 @@ const App = () => {
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
       {/* <Toaster /> */}
       {/* <Navbar /> */}
-      {/* <SearchBar /> */}
+      <SearchBar />
       <Routes>
         <Route path='/' element={<Home />} />
-        {/* <Route path='/collection' element={<Collections />} /> */}
+        {/* <Route path='/collection' element={<Collection />} /> */}
         {/* <Route path='/about' element={<About />} /> */}
         {/* <Route path='/contact' element={<Contact />} /> */}
-        {/* <Route path='/product/:pId' element={<Product />} /> */}
+        {/* <Route path='/product/:productId' element={<Product />} /> */}
         {/* <Route path='/cart' element={<Cart />} /> */}
         {/* <Route path='/login' element={<Login />} /> */}
         {/* <Route path='/place-order' element={<PlaceOrder />} /> */}
